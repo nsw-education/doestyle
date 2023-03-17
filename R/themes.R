9e0aa2aba10280dbb18574c72a477da3fac43cbd
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2023-03-17 14:50:52 +1100

    Remove minor gridmarks

--- R/themes.R
+++ R/themes.R
@@ -7,6 +7,8 @@
 theme_doe <- function(base_size = 12, ...){
   ggplot2::theme_minimal(base_size = base_size, ...) %+replace%
     ggplot2::theme(
-      panel.grid.major.x = element_blank()
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor = element_blank(),
+      complete = TRUE
     )
 }
