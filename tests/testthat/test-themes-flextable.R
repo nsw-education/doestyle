7950a8ecbd69d74b5263c9b348a28e755f6b69ea
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-02-10 17:06:08 +1100

    Relax snapshot test font requirement

--- tests/testthat/test-themes-flextable.R
+++ tests/testthat/test-themes-flextable.R
@@ -5,6 +5,7 @@ test_that("theme_doe_flextable produces consistent HTML output", {
       theme_doe_flextable() |>
       officer::to_html() |>
       # Strip UUID-containing class names
-      gsub("cl-[0-9a-f]{8}", "", x = _ )
+      gsub("cl-[0-9a-f]{8}", "", x = _ ) |>
+      gsub("font-family:'.+?'", "", x = _)
   })
 })
