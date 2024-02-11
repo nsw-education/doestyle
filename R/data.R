098c8558460e51344394994e3070717b4f2c4030
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2024-02-11 18:58:48 +1100

    Document `colourdata`

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Colours provided by `doestyle`
+#'
+#' A dataset containing names, hex and RGB values, and other identifiers for
+#' each colour provided in `doestyle`. Its content can be visualised with
+#' [show_colours()] or further manipulated.
+#'
+#' @seealso [show_colours()]
+#'
+#' @source - NSW Design System
+#'   (https://github.com/digitalnsw/nsw-design-system/)
+#'  - Department of Education Brand guidelines document
+#'   (https://brand.education.nsw.gov.au/)
+"colourdata"
