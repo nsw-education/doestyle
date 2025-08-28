4ad0c2a8fa3290e67d0c9943f960cef6234f06dc
Jonathan McGuire <7284590+JonathanMcGuire@users.noreply.github.com>
2025-08-28 13:40:05 +1000

    Define geom_uperrorbar and geom_downerrorbar

---
+++ R/geom_uperrorbar-geom_downerrorbar.R
@@ -0,0 +1,117 @@
+#' Add either an up or down errorbar to a ggplot2 plot
+#'
+#' Add either the upper or lower portion of an errorbar to a ggplot plot. This
+#' allows the halves of the errorbars to be different colours.
+#'
+#'#' @param ... Other parameters passed to [ggplot2::geom_errorbar()].
+#'
+#' @export
+
+## With thanks to Sean Hughes: https://stackoverflow.com/questions/27585776/error-bars-for-barplot-only-in-one-direction
+
+# Define a new geom for upward error bars
+geom_uperrorbar <- function(mapping = NULL, data = NULL,
+                            stat = "identity", position = "identity",
+                            ...,
+                            na.rm = FALSE,
+                            show.legend = NA,
+                            inherit.aes = TRUE) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomUperrorbar,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+GeomUperrorbar <- ggproto("GeomUperrorbar", Geom,
+                          default_aes = aes(colour = "black", linewidth = 0.5, linetype = 1, width = 0.5,
+                                            alpha = NA),
+
+                          draw_key = draw_key_path,
+                          required_aes = c("x", "y", "ymax"),
+
+                          setup_data = function(data, params) {
+                            data$width <- data$width %||%
+                              params$width %||% (resolution(data$x, FALSE) * 0.9)
+
+                            transform(data,
+                                      xmin = x - width / 2, xmax = x + width / 2, width = NULL
+                            )
+                          },
+
+                          draw_panel = function(data, panel_scales, coord, width = NULL) {
+                            GeomPath$draw_panel(data.frame(
+                              x = as.vector(rbind(data$xmin, data$xmax, NA, data$x,   data$x)),
+                              y = as.vector(rbind(data$ymax, data$ymax, NA, data$ymax, data$y)),
+                              colour = rep(data$colour, each = 5),
+                              alpha = rep(data$alpha, each = 5),
+                              linewidth = rep(data$linewidth, each = 5),
+                              linetype = rep(data$linetype, each = 5),
+                              group = rep(1:(nrow(data)), each = 5),
+                              stringsAsFactors = FALSE,
+                              row.names = 1:(nrow(data) * 5)
+                            ), panel_scales, coord)
+                          }
+)
+
+# Define a new geom for downward error bars
+geom_downerrorbar <- function(mapping = NULL, data = NULL,
+                              stat = "identity", position = "identity",
+                              ...,
+                              na.rm = FALSE,
+                              show.legend = NA,
+                              inherit.aes = TRUE) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomDownerrorbar,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+GeomDownerrorbar <- ggproto("GeomDownerrorbar", Geom,
+                            default_aes = aes(colour = "black", linewidth = 0.5, linetype = 1, width = 0.5,
+                                              alpha = NA),
+
+                            draw_key = draw_key_path,
+
+                            required_aes = c("x", "y", "ymin"),
+
+                            setup_data = function(data, params) {
+                              data$width <- data$width %||%
+                                params$width %||% (resolution(data$x, FALSE) * 0.9)
+
+                              transform(data,
+                                        xmin = x - width / 2, xmax = x + width / 2, width = NULL
+                              )
+                            },
+
+                            draw_panel = function(data, panel_scales, coord, width = NULL) {
+                              GeomPath$draw_panel(data.frame(
+                                x = as.vector(rbind(data$xmin, data$xmax, NA, data$x,   data$x)),
+                                y = as.vector(rbind(data$ymin, data$ymin, NA, data$ymin, data$y)),
+                                colour = rep(data$colour, each = 5),
+                                alpha = rep(data$alpha, each = 5),
+                                linewidth = rep(data$linewidth, each = 5),
+                                linetype = rep(data$linetype, each = 5),
+                                group = rep(1:(nrow(data)), each = 5),
+                                stringsAsFactors = FALSE,
+                                row.names = 1:(nrow(data) * 5)
+                              ), panel_scales, coord)
+                            }
+)
